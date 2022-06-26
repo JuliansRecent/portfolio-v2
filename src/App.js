2c1e949be32483ca6f34367b700e5010d7c4a2e6
JuliansRecent <juliansrecent@gmail.com>
2022-06-26 12:30:00 -0700

    Create and style NavBar content. Import all components into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import NavBar from "./Components/NavBar/NavBar";
+import LandingPage from "./Components/LandingPage/LandingPage"
+import AboutPage from "./Components/AboutPage/AboutPage"
+import ProjectsPage from "./Components/ProjectsPage/ProjectsPage"
+import ContactPage from "./Components/ContactPage/ContactPage"
 
 
 function App() {
   return (
     <div className="app">
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <LandingPage />
+        <AboutPage />
+        <ProjectsPage />
+        <ContactPage />
+      </main>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,11 +2,22 @@ import './NavBar.css';
 
 function NavBar(props) {
 	return (
-		<nav class='navbar fixed-top navbar-light bg-light'>
+		<nav class='navbar fixed-top navbar-light'>
 			<div class='container-fluid'>
-				<a class='navbar-brand' href='#'>
+				<a class='navbar-brand p-2 text-white' href='#'>
 					Julian Segura
 				</a>
+				<ul className='d-flex flex-row align-items-center text-white'>
+					<li className='nav-link p-3'>
+						<a>About</a>
+					</li>
+					<li className='nav-link p-3'>
+						<a>Projects</a>
+					</li>
+					<li className='nav-link p-3'>
+						<a>Contact</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
