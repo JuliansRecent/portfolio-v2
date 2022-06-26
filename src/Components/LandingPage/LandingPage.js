4523a144bbb674bb0148bc817d1872cc41288708
JuliansRecent <juliansrecent@gmail.com>
2022-06-26 13:00:10 -0700

    Add content that is going to be displayed on LandingPage.js. Add styling for hi-statement. Center LandingPage.js vertically.

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,9 +2,11 @@ import './LandingPage.css'
 
 function LandingPage(props) {
     return (
-        <div>
-            
-        </div>
+        <section className='landing-page-container d-flex flex-column justify-content-center'>
+            <h1 className='hi-statement'>Hi, my name is Julian.</h1>
+            <h2 className='im-statement'>I'm a Software Engineer</h2>
+            <p className='i-have-statement'>I have a fous in font-end development and I am currently based in Las Vegas, Nevada.</p>
+        </section>
     );
 }
 
