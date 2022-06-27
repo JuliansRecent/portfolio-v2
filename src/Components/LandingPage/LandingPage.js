db433066deae080ae5d4f776c01fd76e37e8c8f5
JuliansRecent <juliansrecent@gmail.com>
2022-06-27 07:23:34 -0700

    Change padding for main element. Finish first version of styling for landing page.

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage(props) {
         <section className='landing-page-container d-flex flex-column justify-content-center'>
             <h1 className='hi-statement'>Hi, my name is Julian.</h1>
             <h2 className='im-statement'>I'm a Software Engineer</h2>
-            <p className='i-have-statement'>I have a fous in font-end development and I am currently based in Las Vegas, Nevada.</p>
+            <p className='i-have-statement'>I specialize in front-end development and I'm currently based in Las Vegas, Nevada.</p>
         </section>
     );
 }
