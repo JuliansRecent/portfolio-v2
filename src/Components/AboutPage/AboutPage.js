760500013c438d59740ee585e9f2f11bc5075fc5
JuliansRecent <juliansrecent@gmail.com>
2022-06-27 07:46:19 -0700

    Add About Me section with styling, no media queries.

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -1,11 +1,27 @@
 import './AboutPage.css'
+import pictureCollage from '../../Assets/picture-collage.png'
 
 function AboutPage(props) {
     return (
-        <div>
-            
-        </div>
-    );
+			<section className='about-page-container d-flex flex-row justify-content-center'>
+                <div className='collage-container d-flex align-items-center p-3'><img className='collage-picture' src={pictureCollage} alt="" /></div>
+				<aside className='main-text-container d-flex flex-column justify-content-center'>
+                    <h2 className='title'>About Me</h2>
+                    <p className='about-description'>
+                        Hi! My name is Julian Segura. I come from a medical background with
+                        attention to detail, and a passion for helping others. 
+                                        <br /> <br />
+                                        I love bringing innovative ideas to life and pushing my limits everyday. I’m the
+                        happiest when putting my mind to difficult problems and coming out
+                        with optimal solutions. 
+                                        <br /> <br />
+                                        Away from work, I enjoy golfing on the
+                        weekends, spending time with my dogs, and driving fast cars. I also
+                        enjoy rock climbing whenever I get the time to go.
+                    </p>
+                </aside>
+			</section>
+		);
 }
 
 export default AboutPage;
