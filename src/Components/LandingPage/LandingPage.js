1d2b8a45404d225cbb4807e3755b02747048b55f
JuliansRecent <juliansrecent@gmail.com>
2022-08-29 02:14:03 -0700

    Change the LandingPage.js text. Change AboutPage.js text. Update resume.

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -5,9 +5,9 @@ function LandingPage(props) {
     return (
 			<section className='landing-page-container d-flex flex-column justify-content-center'>
 				<h1 className='hi-statement'>Hi, my name is Julian.</h1>
-				<h2 className='im-statement'>Full Stack Developer</h2>
+				<h2 className='im-statement'>Software Engineer</h2>
 				<p className='i-specialize-statement'>
-					I specialize in front-end development and I'm currently based in Las
+					 Currently based in Las
 					Vegas, Nevada.
 				</p>
 				<div className='d-flex gap-3'>
